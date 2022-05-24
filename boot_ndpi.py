47dff7a329a99d0648ade0a2db621993f84db537
Daniel Canuto <danielpscanuto83@gmail.com>
2022-05-24 11:40:48 -0300

    expressoes para base do projeto

--- boot_ndpi.py
+++ boot_ndpi.py
@@ -10,6 +10,15 @@ my_planilha = "pastas_af.xlsx"
 
 data_frame = pd.read_excel(my_planilha)
 
+print(type(data_frame))
+
 
 for id,row in data_frame.iterrows():
     print(f'id: {id} Siape {row["SIAPE"]} - Nome {row["Nome servidor"]}')
+
+plan = pd.read_excel(r"pastas_af.xlsx")
+
+# esse print irar imprimr a colun a Nome servidor na linha 2
+print(plan['Nome servidor'] [2])
+
+print(plan)
